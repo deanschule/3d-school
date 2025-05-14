c521c63dcb8c69e6ed4001d2d7417784062eabd9
zerokrezan <zerokrezan@hotmail.de>
2025-05-14 10:33:53 +0200

    L13SYS-17

---
+++ frontend/services/PathService.js
@@ -0,0 +1,17 @@
+import {Cache as axios} from "three";
+
+const GET_PATH = "http://localhost:8080/route";
+
+async function getPath(startPoint, targetPoint, ) {
+    const path = await axios.get(GET_PATH,{
+        params: {startPoint: startPoint,
+                 targetPoint: targetPoint
+                },
+    });
+    console.log({members: projectMembers})
+    return path.data;
+}
+
+
+
+export { getPath };
