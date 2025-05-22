dd499a209b3684a3991632984e66f3ad7507a9a6
zerokrezan <zerokrezan@hotmail.de>
2025-05-22 14:12:18 +0200

    L13SYS-29 => Crossorigin bugfix

---
+++ backend/src/main/java/de/szut/lf8_starter/controller/PathController.java
@@ -0,0 +1,18 @@
+package de.szut.lf8_starter.controller;
+
+import de.szut.lf8_starter.service.PathfindingService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin(origins = "")
+@RestController()
+@RequestMapping("/path")
+@RequiredArgsConstructor
+public class PathController {
+    private final PathfindingService pathfindingService;
+
+    @GetMapping
+    public String getPath(@RequestParam String startPoint, @RequestParam String targetPoint){
+        return pathfindingService.calculatePath(startPoint,targetPoint);
+    }
+}
