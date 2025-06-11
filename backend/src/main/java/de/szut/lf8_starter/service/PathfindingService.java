e7b61c66d14b8d00b7fa98c7de3ea41f82e87a33
zerokrezan <zerokrezan@hotmail.de>
2025-06-11 12:53:39 +0200

    L13SYS-3: Update room search algorithm

--- backend/src/main/java/de/szut/lf8_starter/service/PathfindingService.java
+++ backend/src/main/java/de/szut/lf8_starter/service/PathfindingService.java
@@ -8,18 +8,57 @@ import java.util.stream.Collectors;
 @Service
 public class PathfindingService {
 
-    private final Map<String, List<String>> roomGraph = Map.of(
-            "001", List.of("002"),
-            "002", List.of("001", "002B"),
-            "002B", List.of("002", "003", "007","008","009","010"),
-            "003", List.of("002B", "004","007","008", "009","010"),
-            "004", List.of("003", "009","010","008","007"),
-            "005", List.of("007", "006"),
-            "007", List.of("005", "006", "002B","008","003","004"),
-            "008", List.of("007", "009", "010", "002B", "003", "004"),
-            "009", List.of("008", "004","003","002B"),
-            "010", List.of("008", "004","003","002B")
-            );
+//    private final Map<String, List<String>> roomGraph = Map.ofEntries(
+//            "001", List.of("002"),
+//            "002", List.of("001", "002B"),
+//            "002B", List.of("002", "003", "007","008","009|010"),
+//            "003", List.of("002B", "004","007","008", "009|010"),
+//            "004", List.of("003", "009|010","010","008","007","mainentrysecond"),
+//            "005|006", List.of("007"),
+//            "007", List.of("005|006", "002B","008","003","004"),
+//            "008", List.of("007", "009|010", "002B", "003", "004"),
+//            "009|010", List.of("008", "004","003","002B","mainentryfirst"),
+//
+//            "042|044", List.of("045"),
+//            "045", List.of("042|044", "secondentry","038","041"),
+//            "046|049", List.of("secondentry","041","038"),
+//            "041", List.of("046|049", "secondentry","045","038"),
+//            "038", List.of("secondentry","045","060","041"),
+//            "060", List.of("035","038"),
+//            "035", List.of("060"),
+//
+//            "mainentrysecond", List.of("004","mainentryfirst","glasscorridor"),
+//            "mainentryfirst", List.of("009|010", "mainentrysecond"),
+//
+//            "glasscorridor", List.of("corridorstairs", "mainentrysecond"),
+//            "corridorstairs", List.of("secondentry","glasscorridor"),
+//            "secondentry", List.of("corridorstairs","041","046|049","045","038")
+
+
+    private final Map<String, List<String>> roomGraph = Map.ofEntries(
+            Map.entry("001", List.of("002")),
+            Map.entry("002", List.of("001", "002B")),
+            Map.entry("002B", List.of("002", "003", "007", "008", "009|010")),
+            Map.entry("003", List.of("002B", "004", "007", "008", "009|010")),
+            Map.entry("004", List.of("003", "009|010", "010", "008", "007", "mainentrysecond")),
+            Map.entry("005|006", List.of("007")),
+            Map.entry("007", List.of("005|006", "002B", "008", "003", "004")),
+            Map.entry("008", List.of("007", "009|010", "002B", "003", "004")),
+            Map.entry("009|010", List.of("008", "004", "003", "002B", "mainentryfirst")),
+            Map.entry("042|044", List.of("045")),
+            Map.entry("045", List.of("042|044", "secondentry", "038", "041")),
+            Map.entry("046|049", List.of("secondentry", "041", "038")),
+            Map.entry("041", List.of("046|049", "secondentry", "045", "038")),
+            Map.entry("038", List.of("secondentry", "045", "060", "041")),
+            Map.entry("060", List.of("035", "038")),
+            Map.entry("035", List.of("060")),
+            Map.entry("mainentrysecond", List.of("004", "mainentryfirst", "glasscorridor")),
+            Map.entry("mainentryfirst", List.of("009|010", "mainentrysecond")),
+            Map.entry("glasscorridor", List.of("corridorstairs", "mainentrysecond")),
+            Map.entry("corridorstairs", List.of("secondentry", "glasscorridor")),
+            Map.entry("secondentry", List.of("corridorstairs", "041", "046|049", "045", "038"))
+    );
+
 
     public String calculatePath(String start, String target){
         return String.join(",",findShortestPath(roomGraph, start, target));
